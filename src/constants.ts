9b2475d55a4415c6be04e751a9cc4501194b3955
Hugo Flotts <hugoflotts@outlook.com>
2023-12-19 05:53:13 -0300

    chore(project): re-activate webpack module federation

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,1 @@
 export const MOVIE_TITLE = 'Harry Potter';
-export const MOVIE_TRANSLATION_KEY = 'movie';
--- src/locales/es.ts
+++ src/locales/es.ts
@@ -1,13 +1,13 @@
 import { Translations } from './en';
 
 export const es: Translations = {
-  HOUSE: 'Casa: ', //
+  HOUSE: 'Casa: ',
   PATRONUS: 'Patronus: ',
-  ACTOR: 'Actor: ', //
-  PREV_PAGE: 'Anterior', //
-  PAGE_INFO: 'Página {{currentPage}} de {{totalPages}}', //
-  NEXT_PAGE: 'Siguiente', //
-  NO_RESULTS_FOUND: 'No se encontraron resultados', //
-  LOADING_TEXT: 'Cargando...', //
-  SEARCH_INPUT_PLACEHOLDER: 'Busca tu personaje favorito', //
+  ACTOR: 'Actor: ',
+  PREV_PAGE: 'Anterior',
+  PAGE_INFO: 'Página {{currentPage}} de {{totalPages}}',
+  NEXT_PAGE: 'Siguiente',
+  NO_RESULTS_FOUND: 'No se encontraron resultados',
+  LOADING_TEXT: 'Cargando...',
+  SEARCH_INPUT_PLACEHOLDER: 'Busca tu personaje favorito',
 };
